ac4d39e27852bc57f2c0ae7f21f81745eab44729
Raphael Belmarez <raphbelmarez@hotmail.com>
2025-03-08 17:47:50 +0000

    implemented new struct and tried to implement usage for function isvalid

--- Solver.cpp
+++ Solver.cpp
@@ -1,6 +1,35 @@
 #include "Solver.h"
+#include <vector>
 
 auto solver::solve(std::vector<std::vector<int> > unsolvedBoard) {
   auto solvedBoard = unsolvedBoard;
   return solvedBoard;
+};
+isValidStruct solver::isValid(int column, int row, int index, int value) {
+  bool checkcolumn = false;
+  bool checkrow = false;
+  isValidStruct checker;
+  //Checking column
+  for(int i = 0; i <= 9; i++) {
+    if (board[row][i] != value) {
+      checkcolumn = true;
+    }
+    else {
+      checker.isFalse = false;
+      checker.tempcolumn = i;
+      checker.temprow = row;
+      return checker;
+    }
+  }
+  //Checking row
+  for (int i = 0; i <= 9; i++) {
+    if (board[i][column] != value) {
+      checkrow = true;
+    }
+    else {
+      checkrow = false;
+    }
+  }
+  board[row][column] = value;
+  return checker;
 };
--- Solver.h
+++ Solver.h
@@ -1,8 +1,13 @@
-
-
 #include <array>
 #include <string>
 #include <vector>
+
+struct isValidStruct {
+  bool isFalse;
+  int tempvalue;
+  int temprow;
+  int tempcolumn;
+};
 
 class solver {
 
@@ -10,8 +15,8 @@ public:
   void readProblem();
   bool isSolved = false;
   auto solve(std::vector<std::vector<int>> unsolvedBoard);
-  bool isValid();
-private:
+  isValidStruct isValid(int column, int row, int index, int value);
+//private:
   std::vector<std::vector<int> > board = {
   {0, 0, 0, 0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0, 0},
